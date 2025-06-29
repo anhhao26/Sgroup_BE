3a994c30b91b420bfa3ef4c83db223e04cf27e37
anhhao26 <anhhaoqk@gmail.com>
2025-06-30 01:04:18 +0700

    Serve README.md at '/' via marked

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { marked } from 'marked';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -13,33 +13,22 @@ import errorHandler from './middlewares/errorHandler.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Hiển thị README.md dưới dạng HTML tại /
+// hiển thị README.md
 app.get('/', (req, res, next) => {
   const mdPath = path.join(__dirname, '../README.md');
-  fs.readFile(mdPath, 'utf8', (err, data) => {
-    if (err) return next(err);
-    const htmlContent = marked(data);
-    res.send(`<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Poll App Documentation</title>
-</head>
-<body>
-  ${htmlContent}
-</body>
-</html>`);
-  });
+  fs.readFile(mdPath, 'utf8', (err, data) => err
+    ? next(err)
+    : res.send(`<!DOCTYPE html><html><head><meta charset="utf-8"><title>Docs</title></head><body>${marked(data)}</body></html>`)
+  );
 });
 
-// Các API routes
+// các route cần có
 app.use('/api', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/polls', pollRoutes);
@@ -48,8 +37,6 @@ app.use('/api/polls', pollRoutes);
 app.use((req, res) => {
   res.status(404).json({ success: false, code: 404, message: 'Route not found' });
 });
-
-// Error handler
 app.use(errorHandler);
 
 export default app;
